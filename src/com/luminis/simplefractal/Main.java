3e4f2b2bb3c09d33a3b11eaee6df6d5601b93b97
Paul Ebeling <paul.ebeling@luminis.eu>
2016-04-13 17:27:58 +0200

    Image scales with main window. Colored interior instead of black

--- src/com/luminis/simplefractal/Main.java
+++ src/com/luminis/simplefractal/Main.java
@@ -17,10 +17,11 @@ public class Main extends Application {
 	private double xCenter = xMin + xMax;
 	private double yCenter = yMin + yMax;
 
-	private int canvasHorizontal = 600;
-	private int canvasVertical = 500;
+	private int canvasWidth = 600;
+	private int canvasHeight = 500;
+	private double aspectRatio = canvasWidth / canvasHeight;
 
-	private double adjustedRatio = Math.min(canvasHorizontal / (xMax - xMin), canvasVertical / (yMax - yMin));
+	private double adjustedRatio = Math.min(canvasWidth / (xMax - xMin), canvasHeight / (yMax - yMin));
 
 	@Override
 	public void start(Stage primaryStage) throws Exception{
@@ -29,19 +30,23 @@ public class Main extends Application {
 		primaryStage.setTitle("Simple Mandelbrot Fractal");
 		Group root = new Group();
 
-		WritableImage image = new WritableImage(canvasHorizontal, canvasVertical);
+		WritableImage image = new WritableImage(canvasWidth, canvasHeight);
 		PixelWriter writer = image.getPixelWriter();
 
 		Timer timer = new Timer();
 		timer.start();
-		fractal(writer, 50);
+		fractal(writer, 100);
 		timer.stop();
 		System.out.println(timer);
 
 		ImageView imageView = new ImageView(image);
+		imageView.setPreserveRatio(true);
+
 		root.getChildren().add(imageView);
-		primaryStage.setScene(new Scene(root));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
 		primaryStage.show();
+		imageView.fitWidthProperty().bind(scene.widthProperty());
 	}
 
 	public static void main(String[] args) {
@@ -49,17 +54,18 @@ public class Main extends Application {
 	}
 
 	private void adjustAspectRatio(){
-		xMin = -canvasHorizontal / adjustedRatio / 2 + xCenter;
-		xMax = canvasHorizontal / adjustedRatio / 2 + xCenter;
+		xMin = -canvasWidth / adjustedRatio / 2 + xCenter;
+		xMax = canvasWidth / adjustedRatio / 2 + xCenter;
 
-		yMin = -canvasVertical / adjustedRatio / 2 + yCenter;
-		yMax = canvasVertical / adjustedRatio / 2 + yCenter;
+		yMin = -canvasHeight / adjustedRatio / 2 + yCenter;
+		yMax = canvasHeight / adjustedRatio / 2 + yCenter;
 	}
 
 	private void fractal(PixelWriter writer, int iterationDepth){
 		Color color;
 		double tmp;
 		double x, y;
+		double limit = 5.0;
 
 		double xOrbit;
 		double yOrbit;
@@ -68,9 +74,9 @@ public class Main extends Application {
 		double xOrbitSq;
 		double yOrbitSq;
 
-		for(int xCanvas = 0; xCanvas < canvasHorizontal; xCanvas++) {
+		for(int xCanvas = 0; xCanvas < canvasWidth; xCanvas++) {
 			x = xCanvas / adjustedRatio + xMin;
-			for(int yCanvas = 0; yCanvas < canvasVertical; yCanvas++) {
+			for(int yCanvas = 0; yCanvas < canvasHeight; yCanvas++) {
 				y = yCanvas / adjustedRatio + yMin;
 
 				xOrbit = 0;
@@ -80,7 +86,7 @@ public class Main extends Application {
 				yOrbitSq = 0;
 
 				double i;
-				for (i = 0; i < iterationDepth && xOrbitSq + yOrbitSq < 10; i++) {
+				for (i = 0; i < iterationDepth && xOrbitSq + yOrbitSq < limit; i++) {
 					xOrbitSq = xOrbit * xOrbit;
 					yOrbitSq = yOrbit * yOrbit;
 
@@ -90,13 +96,14 @@ public class Main extends Application {
 				}
 
 				if(i < iterationDepth) {
-					tmp = 1 - i / iterationDepth;
+					tmp = 1 - Math.sqrt(i) / Math.sqrt(iterationDepth);
 					color = Color.color(tmp, tmp * tmp, tmp * tmp * tmp, 1.0);
 				} else {
-					color = Color.BLACK;
-//					tmp = Math.min(xOrbitSq + yOrbitSq, 10) / 10;
-//					tmp = Math.sqrt(tmp);
-//					color = Color.color(tmp, tmp * tmp, tmp * tmp * tmp, 1.0);
+					//color = Color.BLACK;
+					tmp = Math.min(xOrbitSq + yOrbitSq, limit) / limit;
+					tmp = Math.sqrt(tmp);
+					if (tmp > 0.9) tmp = 0;
+					color = Color.color(tmp * tmp, 0, tmp, 1.0);
 				}
 				writer.setColor(xCanvas, yCanvas, color);
 			}
