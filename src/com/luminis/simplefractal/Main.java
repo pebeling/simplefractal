19987e33229fe7e47108880ae62255565b469e68
Paul Ebeling <paul.ebeling@luminis.eu>
2016-04-15 10:39:25 +0200

    Added scrolling, panning, resetting and a label with coordinates

--- src/com/luminis/simplefractal/Main.java
+++ src/com/luminis/simplefractal/Main.java
@@ -5,18 +5,13 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleFloatProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
-import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -25,118 +20,87 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	private static final int MIN_PIXELS = 10;
+	private Rectangle2D fractalBox = new Rectangle2D(-2.0, -1.2, 3.2, 2.4);
 
-	private double xMin = -2;
-	private double xMax = 1.2;
-	private double yMin = -1.2;
-	private double yMax = 1.2;
-	private double xCenter = xMin + xMax;
-	private double yCenter = yMin + yMax;
-	private SimpleFloatProperty currentX;
-	private SimpleFloatProperty currentY;
+	private int imageWidth = 5120;
+	private int imageHeight = 3200;
 
-	private int canvasWidth = 5120;
-	private int canvasHeight = 3200;
+	private static final int MIN_PIXELS = 50;
 
-	private double adjustedRatio = Math.min(canvasWidth / (xMax - xMin), canvasHeight / (yMax - yMin));
+	private double ratio = Math.min(imageWidth / fractalBox.getWidth(), imageHeight / fractalBox.getHeight());
+
+	private Rectangle2D adjustedFractalBox = new Rectangle2D(
+			-imageWidth / ratio / 2 + fractalBox.getMinX() + fractalBox.getMaxX(),
+			-imageHeight / ratio / 2 + fractalBox.getMinY() + fractalBox.getMaxY(),
+			imageWidth / ratio,
+			imageHeight / ratio);
 
 	@Override
 	public void start(Stage primaryStage)
 	{
-		currentX = new SimpleFloatProperty(0);
-		currentY = new SimpleFloatProperty(0);
-
-		adjustAspectRatio();
-		WritableImage image = new WritableImage(5120,3200);
+		WritableImage image = new WritableImage(imageWidth, imageHeight);
 		PixelWriter writer = image.getPixelWriter();
-		double width = image.getWidth();
-		double height = image.getHeight();
 
 		ImageView imageView = new ImageView(image);
 		imageView.setPreserveRatio(true);
-		reset(imageView, width, height);
+		imageView.setViewport(new Rectangle2D(0, 0, imageWidth, imageHeight));
 
-		ObjectProperty<Point2D> mouseDown = new SimpleObjectProperty<>();
-
+		SimpleFloatProperty currentX = new SimpleFloatProperty(0);
+		SimpleFloatProperty currentY = new SimpleFloatProperty(0);
 		imageView.setOnMouseMoved(e -> {
-			Point2D imagePoint = imageViewToImage(imageView, new Point2D(e.getX(), e.getY()));
-			Point2D realPoint = imageToReal(imagePoint);
+			Point2D imagePoint = imageViewToImageCoordinates(imageView, new Point2D(e.getX(), e.getY()));
+			Point2D realPoint = imageToFractalCoordinates(imagePoint);
 			currentX.setValue(realPoint.getX());
 			currentY.setValue(realPoint.getY());
 		});
-
 		Label coordinates = new Label();
-		coordinates.textProperty().bind(Bindings.concat("X: ", currentX.asString(), ", Y: ", currentY.asString()));
+		coordinates.textProperty().bind(Bindings.concat("( ", currentX.asString(), ", ", currentY.asString(), " )"));
 
+		ObjectProperty<Point2D> mouseDown = new SimpleObjectProperty<>();
 		imageView.setOnMousePressed(e -> {
-
-			Point2D mousePress = imageViewToImage(imageView, new Point2D(e.getX(), e.getY()));
+			Point2D mousePress = imageViewToImageCoordinates(imageView, new Point2D(e.getX(), e.getY()));
 			mouseDown.set(mousePress);
 		});
-
 		imageView.setOnMouseDragged(e -> {
-			Point2D dragPoint = imageViewToImage(imageView, new Point2D(e.getX(), e.getY()));
+			Point2D dragPoint = imageViewToImageCoordinates(imageView, new Point2D(e.getX(), e.getY()));
 			shift(imageView, dragPoint.subtract(mouseDown.get()));
-			mouseDown.set(imageViewToImage(imageView, new Point2D(e.getX(), e.getY())));
+			mouseDown.set(imageViewToImageCoordinates(imageView, new Point2D(e.getX(), e.getY())));
+		});
+
+		imageView.setOnMouseClicked(e -> {
+			if (e.getClickCount() == 2) {
+				imageView.setViewport(new Rectangle2D(0, 0, imageWidth, imageHeight));
+			}
 		});
 
 		imageView.setOnScroll(e -> {
 			double delta = e.getDeltaY();
 			Rectangle2D viewport = imageView.getViewport();
 
-			double scale = clamp(Math.pow(1.01, delta),
-
-					// don't scale so we're zoomed in to fewer than MIN_PIXELS in any direction:
+			double scale = clamp(Math.pow(0.99, delta), // zoom speed
 					Math.min(MIN_PIXELS / viewport.getWidth(), MIN_PIXELS / viewport.getHeight()),
-
-					// don't scale so that we're bigger than image dimensions:
-					Math.max(width / viewport.getWidth(), height / viewport.getHeight())
-
+					Math.max(imageWidth / viewport.getWidth(), imageHeight / viewport.getHeight())
 			);
 
-			Point2D mouse = imageViewToImage(imageView, new Point2D(e.getX(), e.getY()));
+			Point2D mouse = imageViewToImageCoordinates(imageView, new Point2D(e.getX(), e.getY()));
 
 			double newWidth = viewport.getWidth() * scale;
 			double newHeight = viewport.getHeight() * scale;
 
-			// To keep the visual point under the mouse from moving, we need
-			// (x - newViewportMinX) / (x - currentViewportMinX) = scale
-			// where x is the mouse X coordinate in the image
-
-			// solving this for newViewportMinX gives
-
-			// newViewportMinX = x - (x - currentViewportMinX) * scale
-
-			// we then clamp this value so the image never scrolls out
-			// of the imageview:
-
-			double newMinX = clamp(mouse.getX() - (mouse.getX() - viewport.getMinX()) * scale,
-					0, width - newWidth);
-			double newMinY = clamp(mouse.getY() - (mouse.getY() - viewport.getMinY()) * scale,
-					0, height - newHeight);
+			double newMinX = clamp(mouse.getX() - (mouse.getX() - viewport.getMinX()) * scale, 0, imageWidth - newWidth);
+			double newMinY = clamp(mouse.getY() - (mouse.getY() - viewport.getMinY()) * scale, 0, imageHeight - newHeight);
 
 			imageView.setViewport(new Rectangle2D(newMinX, newMinY, newWidth, newHeight));
 		});
 
-		imageView.setOnMouseClicked(e -> {
-			if (e.getClickCount() == 2) {
-				reset(imageView, width, height);
-			}
-		});
-
-		fractal(writer, 250);
-
-		HBox buttons = createButtons(width, height, imageView);
-		Tooltip tooltip = new Tooltip("Scroll to zoom, drag to pan");
-		Tooltip.install(buttons, tooltip);
+		fractal(writer, 500);
 
 		Pane container = new Pane(imageView);
 		container.setPrefSize(950, 600);
 
 		imageView.fitWidthProperty().bind(container.widthProperty());
 		imageView.fitHeightProperty().bind(container.heightProperty());
-		VBox root = new VBox(container, buttons, coordinates);
+		VBox root = new VBox(container, coordinates);
 		root.setFillWidth(true);
 		VBox.setVgrow(container, Priority.ALWAYS);
 
@@ -145,22 +109,6 @@ public class Main extends Application {
 		primaryStage.show();
 	}
 
-	private HBox createButtons(double width, double height, ImageView imageView) {
-		Button full = new Button("Full view");
-		full.setOnAction(e -> reset(imageView, width, height));
-		HBox buttons = new HBox(10, full);
-		buttons.setAlignment(Pos.CENTER);
-		buttons.setPadding(new Insets(10));
-		return buttons;
-	}
-
-	// reset to the top left:
-	private void reset(ImageView imageView, double width, double height) {
-		imageView.setViewport(new Rectangle2D(0, 0, width, height));
-	}
-
-	// shift the viewport of the imageView by the specified delta, clamping so
-	// the viewport does not move off the actual image:
 	private void shift(ImageView imageView, Point2D delta) {
 		Rectangle2D viewport = imageView.getViewport();
 
@@ -177,7 +125,6 @@ public class Main extends Application {
 	}
 
 	private double clamp(double value, double min, double max) {
-
 		if (value < min)
 			return min;
 		if (value > max)
@@ -185,8 +132,7 @@ public class Main extends Application {
 		return value;
 	}
 
-	// convert mouse coordinates in the imageView to coordinates in the actual image:
-	private Point2D imageViewToImage(ImageView imageView, Point2D imageViewCoordinates) {
+	private Point2D imageViewToImageCoordinates(ImageView imageView, Point2D imageViewCoordinates) {
 		double xProportion = imageViewCoordinates.getX() / imageView.getBoundsInLocal().getWidth();
 		double yProportion = imageViewCoordinates.getY() / imageView.getBoundsInLocal().getHeight();
 
@@ -196,25 +142,17 @@ public class Main extends Application {
 				viewport.getMinY() + yProportion * viewport.getHeight());
 	}
 
-	private Point2D imageToReal(Point2D imageCoordinates) {
-		double xProportion = imageCoordinates.getX() / canvasWidth;
-		double yProportion = imageCoordinates.getY() / canvasHeight;
+	private Point2D imageToFractalCoordinates(Point2D imageCoordinates) {
+		double xProportion = imageCoordinates.getX() / imageWidth;
+		double yProportion = imageCoordinates.getY() / imageHeight;
 
 		return new Point2D(
-				xMin + xProportion * ( xMax - xMin ),
-				yMax - yProportion * ( yMax - yMin));
+				adjustedFractalBox.getMinX() + xProportion * adjustedFractalBox.getWidth(),
+				adjustedFractalBox.getMaxY() - yProportion * adjustedFractalBox.getHeight());
 	}
 
 	public static void main(String[] args) {
 		launch(args);
-	}
-
-	private void adjustAspectRatio(){
-		xMin = -canvasWidth / adjustedRatio / 2 + xCenter;
-		xMax = canvasWidth / adjustedRatio / 2 + xCenter;
-
-		yMin = -canvasHeight / adjustedRatio / 2 + yCenter;
-		yMax = canvasHeight / adjustedRatio / 2 + yCenter;
 	}
 
 	private void fractal(PixelWriter writer, int iterationDepth){
@@ -230,10 +168,10 @@ public class Main extends Application {
 		double xOrbitSq;
 		double yOrbitSq;
 
-		for(int xCanvas = 0; xCanvas < canvasWidth; xCanvas++) {
-			x = xCanvas / adjustedRatio + xMin;
-			for(int yCanvas = 0; yCanvas < canvasHeight; yCanvas++) {
-				y = yCanvas / adjustedRatio + yMin;
+		for(int xCanvas = 0; xCanvas < imageWidth; xCanvas++) {
+			x = xCanvas / ratio + adjustedFractalBox.getMinX();
+			for(int yCanvas = 0; yCanvas < imageHeight; yCanvas++) {
+				y = -yCanvas / ratio + adjustedFractalBox.getMaxY();
 
 				xOrbit = 0;
 				yOrbit = 0;
